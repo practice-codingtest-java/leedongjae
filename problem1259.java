408d36e28ced1fc37b22cc1d6550c1652cb6d2b5
leeedongjaee <harold306122@gmail.com>
2025-04-03 09:35:01 +0900

    1주차 풀이(1259번)

---
+++ problem1259.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class problem1259 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        while(true){
+            String num=br.readLine();
+            if(num.equals("0")) break;
+            String reversed = new StringBuilder(num).reverse().toString();
+            if(num.equals(reversed)){
+                sb.append("yes\n");
+            }else{
+                sb.append("no\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
