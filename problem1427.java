bb628471effbbe57cff5cc0ce8edc783c06abcee
leeedongjaee <harold306122@gmail.com>
2025-04-06 21:42:10 +0900

    1주차 문제풀이 (1427번)

--- problem1427.java
+++ problem1427.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class problem1427 {
+import java.io.IOException;
+import java.util.*;
+import java.io.*;
+public class problem1427 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String s=br.readLine();
+        char[] arr=s.toCharArray();
+        Arrays.sort(arr);
+        StringBuilder sb=new StringBuilder(new String(arr));
+        System.out.println(sb.reverse());
+    }
 }
