3fab9d20f85e527bb2a5d5f432d8a3d3d876bd41
leeedongjaee <harold306122@gmail.com>
2025-04-06 21:42:38 +0900

    1주차 문제풀이 (9093번)

--- problem9093.java
+++ problem9093.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class problem9093 {
+import java.util.*;
+import java.io.*;
+
+public class problem9093 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int T=Integer.parseInt(br.readLine());
+        for(int i=0;i<T;i++){
+            String[] arr=br.readLine().split(" ");
+            StringBuilder sb=new StringBuilder();
+
+            for(String content:arr){
+                sb.append(new StringBuilder(content).reverse());
+                sb.append(" ");
+            }
+            System.out.println(sb.toString());
+        }
+    }
 }
