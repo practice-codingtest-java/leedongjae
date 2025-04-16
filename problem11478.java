759d2d6907513d80b2a3ce54d67f2dfdcfd87dd8
leeedongjaee <harold306122@gmail.com>
2025-04-16 16:26:00 +0900

    3주차 문제풀이 (11478번)

--- problem11478.java
+++ problem11478.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class problem11478 {
+import java.util.*;
+import java.io.*;
+public class problem11478 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String s=br.readLine();
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                set.add(s.substring(i, j));
+            }
+        }
+        System.out.println(set.size());
+    }
 }
