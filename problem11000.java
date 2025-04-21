94d96bcd4779c541dfbac435a02abc0b388a8d37
leeedongjaee <harold306122@gmail.com>
2025-04-21 17:50:19 +0900

    2주차 문제풀이(11000번)

---
+++ problem11000.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+public class problem11000 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        PriorityQueue<int[]> pq = new PriorityQueue<>(((o1, o2) -> {
+            if(o1[0] == o2[0]){
+                return o1[1] - o2[1];
+            }else{
+                return o1[0] - o2[0];
+            }
+        }));
+        for(int i=0;i<n;i++){
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            pq.offer(new int[]{start, end});
+        }
+
+        PriorityQueue<Integer> classes=new PriorityQueue<>();
+        classes.offer(0);
+        while(!pq.isEmpty()){
+            int arr[]=pq.poll();
+            if(classes.peek() <= arr[0]){
+                classes.poll();
+            }
+            classes.offer(arr[1]);
+        }
+        System.out.println(classes.size());
+    }
+}
