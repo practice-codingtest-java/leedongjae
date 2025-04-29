20a50a10dc3ba1c975b5c5ec66f863782ad9b9eb
leeedongjaee <harold306122@gmail.com>
2025-04-29 09:24:30 +0900

    4주차 문제풀이(11725번)

--- problem11725.java
+++ problem11725.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class problem11725 {
+import java.io.*;
+import java.util.*;
+
+public class problem11725 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        int[] parent = new int[n + 1];
+        boolean[] visited = new boolean[n + 1];
+
+        bfs(graph, parent, visited, 1);
+
+        for (int i = 2; i <= n; i++) {
+            sb.append(parent[i]).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static void bfs(ArrayList<ArrayList<Integer>> graph, int[] parent, boolean[] visited, int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+
+            for (int next : graph.get(node)) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    parent[next] = node;
+                    queue.offer(next);
+                }
+            }
+        }
+    }
 }
