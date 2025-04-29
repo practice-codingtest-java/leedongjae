7159d37f213509abb50ab8fbffa6b841139319fc
leeedongjaee <harold306122@gmail.com>
2025-04-29 09:27:47 +0900

    4주차 문제풀이(7576번)

--- problem7576.java
+++ problem7576.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class problem7576 {
+import java.util.*;
+import java.io.*;
+
+public class problem7576 {
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static int count = 0;
+    static int N, M;
+    static Queue<int[]> q = new LinkedList<>();
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 1) {
+                    q.add(new int[]{i, j});
+                }
+            }
+        }
+
+        int result = bfs();
+        System.out.println(result);
+    }
+
+    static int bfs() {
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int x = now[0];
+            int y = now[1];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                    if (map[nx][ny] == 0) {
+                        q.add(new int[]{nx, ny});
+                        map[nx][ny] = map[x][y] + 1;
+                    }
+                }
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0) {
+                    return -1;
+                }
+                max = Math.max(max, map[i][j]);
+            }
+        }
+        return max - 1;
+    }
 }
